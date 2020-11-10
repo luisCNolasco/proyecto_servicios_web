e370b80fb3e54fb8fc2dab5bacc5f4cb719e538e
Luis Carranza <luiscarranza3395@gmail.com>
2020-11-10 17:45:21 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto-Veterinaria/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_Veterinaria.Models;
+
+namespace Proyecto_Veterinaria.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult IniciarSesion(string usu,string pass)
+        {
+            if(usu.Equals("hola") && pass.Equals("123"))
+            {
+                return View();
+            }
+            else
+            {
+                ViewBag.mensaje = "Usuario o contraseña incorrecta";
+                return RedirectToAction("InicioSesion","Home");
+            }
+            
+        }
+    }
+}
---
+++ Proyecto-Veterinaria/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_Veterinaria.Models
+{
+    public class Usuario
+    {
+        public string codigo { get; set; }
+        public string password { get; set; }
+    }
+}
